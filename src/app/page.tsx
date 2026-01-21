fc2729643dd510f7e9f3d978fd6254e9510f47f4
Arjay18 <arjaydomingo008@gmail.com>
2026-01-21 10:57:47 +0800

    test new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,38 +10,53 @@ import { FadeIn } from "@/components/animations/FadeIn";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <FadeIn direction="up" delay={0.2}>
-        <HomeSlider />
-      </FadeIn>
+    <>
+      {/* Organization Schema Markup for Google Logo */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "San Jose Antique MPC",
+            "url": "https://sanjoseantiquempc.com./",
+            "logo": "https://sanjoseantiquempc.com./images/433653723_8032419583452138_6238720083292977796_n.jpg"
+          })
+        }}
+      />
+      <div className="flex min-h-screen flex-col">
+        <FadeIn direction="up" delay={0.2}>
+          <HomeSlider />
+        </FadeIn>
 
-      <FadeIn direction="up" delay={0.4}>
-        <AboutUs />
-      </FadeIn>
+        <FadeIn direction="up" delay={0.4}>
+          <AboutUs />
+        </FadeIn>
 
-      <FadeIn direction="up" delay={0.8}>
-        <ServicesShowcase />
-      </FadeIn>
+        <FadeIn direction="up" delay={0.8}>
+          <ServicesShowcase />
+        </FadeIn>
 
-      <FadeIn direction="up" delay={1.0}>
-        <HowToBecomeMember />
-      </FadeIn>
+        <FadeIn direction="up" delay={1.0}>
+          <HowToBecomeMember />
+        </FadeIn>
 
-      <FadeIn direction="up" delay={1.2}>
-        <Testimonials />
-      </FadeIn>
+        <FadeIn direction="up" delay={1.2}>
+          <Testimonials />
+        </FadeIn>
 
-      <FadeIn direction="up" delay={1.4}>
-        <LatestProjects />
-      </FadeIn>
+        <FadeIn direction="up" delay={1.4}>
+          <LatestProjects />
+        </FadeIn>
 
-      <FadeIn direction="up" delay={1.6}>
-        <PMESSchedule />
-      </FadeIn>
+        <FadeIn direction="up" delay={1.6}>
+          <PMESSchedule />
+        </FadeIn>
 
-      <FadeIn direction="up" delay={1.8}>
-        <ContactForm />
-      </FadeIn>
-    </div>
+        <FadeIn direction="up" delay={1.8}>
+          <ContactForm />
+        </FadeIn>
+      </div>
+    </>
   );
 }
