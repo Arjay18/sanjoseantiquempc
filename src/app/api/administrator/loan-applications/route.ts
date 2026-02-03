7e85122041221b340f37dceea26c99a142b32ddd
Arjay18 <arjaydomingo008@gmail.com>
2026-02-03 11:04:27 +0800

    fix error build

--- src/app/api/administrator/loan-applications/route.ts
+++ src/app/api/administrator/loan-applications/route.ts
@@ -30,7 +30,7 @@ export async function GET(request: NextRequest) {
     });
 
 
-    let where: any = {};
+    const where: any = {};
     if (status && status !== 'all') {
       where.status = status;
     }
