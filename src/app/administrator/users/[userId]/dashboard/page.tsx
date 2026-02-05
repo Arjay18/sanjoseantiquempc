b52efbe359aa333ba6df1baff63fef3a2b0068bc
Arjay18 <arjaydomingo008@gmail.com>
2026-02-05 14:47:34 +0800

    `Updated Page component to use routeParams instead of params`

--- src/app/administrator/users/[userId]/dashboard/page.tsx
+++ src/app/administrator/users/[userId]/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import UserDashboardClient from "./UserDashboardClient";
 
-export default async function Page({ params }: { params: { userId: string } }) {
-	return <UserDashboardClient userId={params.userId} />;
+export default async function Page({ params: routeParams }: { params: { userId: string } }) {
+	return <UserDashboardClient userId={routeParams.userId} />;
 }
