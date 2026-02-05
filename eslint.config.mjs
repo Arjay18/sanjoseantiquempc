9814f5a58d2d3ad3ebcaadd04546820b9e9cae5a
Arjay18 <arjaydomingo008@gmail.com>
2026-02-05 13:54:18 +0800

    `Updated dependencies in package-lock.json and package.json`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
   {
+    extends: ["next/core-web-vitals"],
     ignores: [
       "node_modules/**",
       ".next/**",
@@ -29,5 +18,3 @@ const eslintConfig = [
     },
   },
 ];
-
-export default eslintConfig;
