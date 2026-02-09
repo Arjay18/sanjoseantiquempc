830f96cd9a4d2f9f2643eadade97735dd3b11cc0
Arjay18 <arjaydomingo008@gmail.com>
2026-02-09 10:39:43 +0800

    `Update DashboardLayout component: change max-width and padding of main div`

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -63,7 +63,7 @@ export default function DashboardLayout({
             </header>
             {/* Add client component for personalized header */}
             <main className="flex-grow pt-6 pb-8 bg-gray-50">
-              <div className="max-w-4xl mx-auto px-4">
+              <div className="w-full px-0">
                 {children}
               </div>
             </main>
