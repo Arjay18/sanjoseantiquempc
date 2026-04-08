11da863c3f36534f57232a68dba7b87e75741f72
Arjay18 <arjaydomingo008@gmail.com>
2026-04-08 10:43:27 +0800

    new updated pictures

--- src/app/head-office-management-staff/page.tsx
+++ src/app/head-office-management-staff/page.tsx
@@ -7,48 +7,48 @@ export default function HeadOfficeManagementStaffPage() {
 
   const staffData = {
     mainOffice: [
-      { name: "RODELYN I. VERA CRUZ-BERTO", position: "General Manager", icon: Crown, priority: 1, color: "from-amber-500 to-yellow-600", image: "/staff/rodelvn-berto.jpg" }
+      { name: "RODELYN I. VERA CRUZ-BERTO", position: "General Manager", icon: Crown, priority: 1, color: "from-amber-500 to-yellow-600", image: "/Management%20and%20Staff/Main%20Office/Admin/RODELYN%20I.%20VERA%20CRUZ%20BERTO.jpg" }
     ],
     administrativeSection: [
-      { name: "PHOEBE T. SASOTA", position: "Clerk", icon: FileText, color: "from-green-500 to-green-600", image: "/staff/phoebe-sasota.jpg" },
-      { name: "MERVIN A. JONELA", position: "Clerk", icon: FileText, color: "from-blue-500 to-blue-600", image: "/staff/mervin-jonela.jpg" },
-      { name: "ROCKY C. SEBAN", position: "Driver/ Maintenance Clerk", icon: Settings, color: "from-purple-500 to-purple-600", image: "/staff/rocky-seban.jpg" },
-      { name: "JUDELYN M. SANTILLAN", position: "Clerk", icon: FileText, color: "from-yellow-500 to-yellow-600", image: "/staff/judelyn-santillan.jpg" },
-      { name: "MARILOU T. ANTONIO", position: "HR", icon: UserCheck, color: "from-pink-500 to-pink-600", image: "/staff/myca-pagunaling.jpg" },
-      { name: "ARJAY D. DOMINGO", position: "IT JO", icon: UserCheck, color: "from-pink-500 to-pink-600", image: "/staff/myca-pagunaling.jpg" }
+      { name: "PHOEBE T. SASOTA", position: "Clerk", icon: FileText, color: "from-green-500 to-green-600", image: "/Management%20and%20Staff/Main%20Office/Admin/PHOEBE%20T.%20SASOTA.jpg" },
+      { name: "MERVIN A. JONELA", position: "Clerk", icon: FileText, color: "from-blue-500 to-blue-600", image: "/Management%20and%20Staff/Main%20Office/Admin/MERVIN%20A.%20JONELA.jpg" },
+      { name: "ROCKY C. SEBAN", position: "Driver/ Maintenance Clerk", icon: Settings, color: "from-purple-500 to-purple-600", image: "/Management%20and%20Staff/Main%20Office/Admin/ROCKY%20C.%20SEBAN.jpg" },
+      { name: "JUDELYN M. SANTILLAN", position: "Clerk", icon: FileText, color: "from-yellow-500 to-yellow-600", image: "" },
+      { name: "MARILOU T. ANTONIO", position: "HR", icon: UserCheck, color: "from-pink-500 to-pink-600", image: "/Management%20and%20Staff/Main%20Office/Admin/MARILOU%20T.%20ANTONIO.jpg" },
+      { name: "ARJAY D. DOMINGO", position: "IT JO", icon: UserCheck, color: "from-pink-500 to-pink-600", image: "/Management%20and%20Staff/Main%20Office/Admin/ARJAY%20DOMINGO.jpg" }
     ],
     accountingSection: [
-      { name: "GERALDINE R. CATALDAVAN", position: "Accountant", icon: Calculator, color: "from-yellow-500 to-yellow-600", image: "/staff/geraldine-cataldavan.jpg" },
-      { name: "KAREN KAYE G. JUANILLO", position: "Bookkeeper", icon: BookOpen, color: "from-purple-500 to-purple-600", image: "/staff/karen-juanillo.jpg" },
-      { name: "SUNSHINE E. LABRADOR", position: "Bookkeeper", icon: FileText, color: "from-pink-500 to-pink-600", image: "/staff/sunshine-labrador.jpg" },
-      { name: "MYCA JANE PAGUNALING", position: "Accounting Clerk", icon: FileText, color: "from-pink-500 to-pink-600", image: "/staff/sunshine-labrador.jpg" }
+      { name: "GERALDINE R. CATALDAVAN", position: "Accountant", icon: Calculator, color: "from-yellow-500 to-yellow-600", image: "/Management%20and%20Staff/Main%20Office/Accounting/geraldine%20cataldavan.png" },
+      { name: "KAREN KAYE G. JUANILLO", position: "Bookkeeper", icon: BookOpen, color: "from-purple-500 to-purple-600", image: "/Management%20and%20Staff/Main%20Office/Accounting/karen%20kaye%20bacanto.png" },
+      { name: "SUNSHINE E. LABRADOR", position: "Bookkeeper", icon: FileText, color: "from-pink-500 to-pink-600", image: "/Management%20and%20Staff/Main%20Office/Accounting/sunshine%20labrador2.png" },
+      { name: "MYCA JANE PAGUNALING", position: "Accounting Clerk", icon: FileText, color: "from-pink-500 to-pink-600", image: "" }
     ],
     tellerSection: [
-      { name: "MICHELLE MARIE REQUIRON", position: "Clerk, JO", icon: UserCheck, color: "from-purple-500 to-purple-600", image: "/staff/michelle-requiron.jpg" },
-      { name: "CRISTINE JOY P. VIRGO", position: "Teller", icon: DollarSign, color: "from-indigo-500 to-indigo-600", image: "/staff/cristine-virgo.jpg" },
-      { name: "DARYL B. BETITA", position: "Teller", icon: DollarSign, color: "from-red-500 to-red-600", image: "/staff/daryl-betita.jpg" },
-      { name: "ERYNE MAE T. MIQUELA", position: "Cashier", icon: Target, color: "from-teal-500 to-teal-600", image: "/staff/eryne-miquela.jpg" },
-      { name: "GENEROSO S. UMBAT, JR.", position: "Teller", icon: DollarSign, color: "from-orange-500 to-orange-600", image: "/staff/generoso-umbat.jpg" },
-      { name: "FRITZ INOT", position: "Account Officer, JO", icon: Briefcase, color: "from-cyan-500 to-cyan-600", image: "/staff/fritz-inot.jpg" }
+      { name: "MICHELLE MARIE REQUIRON", position: "Clerk, JO", icon: UserCheck, color: "from-purple-500 to-purple-600", image: "/Management%20and%20Staff/Main%20Office/Cash%20Section/michelle%20requiron.png" },
+      { name: "CRISTINE JOY P. VIRGO", position: "Teller", icon: DollarSign, color: "from-indigo-500 to-indigo-600", image: "/Management%20and%20Staff/Main%20Office/Cash%20Section/Kristine%20Joy%20Virgo.png" },
+      { name: "DARYL B. BETITA", position: "Teller", icon: DollarSign, color: "from-red-500 to-red-600", image: "/Management%20and%20Staff/Main%20Office/Cash%20Section/daryl%20betita.png" },
+      { name: "ERYNE MAE T. MIQUELA", position: "Cashier", icon: Target, color: "from-teal-500 to-teal-600", image: "/Management%20and%20Staff/Main%20Office/Cash%20Section/eryne%20may%20miquela1.png" },
+      { name: "GENEROSO S. UMBAT, JR.", position: "Teller", icon: DollarSign, color: "from-orange-500 to-orange-600", image: "/Management%20and%20Staff/Main%20Office/Cash%20Section/generoso%20umbat.png" },
+      { name: "FRITZ INOT", position: "Account Officer, JO", icon: Briefcase, color: "from-cyan-500 to-cyan-600", image: "" }
     ],
     accountOfficerSection: [
-      { name: "GLENN D. MISAJON", position: "Account Officer", icon: Briefcase, color: "from-indigo-500 to-indigo-600", image: "/staff/glenn-misajon.jpg" },
-      { name: "LAMBERT S. LABIAO", position: "Account Officer", icon: Briefcase, color: "from-red-500 to-red-600", image: "/staff/lambert-labiao.jpg" },
-      { name: "CHARLO C. CASIDSID", position: "Account Officer", icon: Briefcase, color: "from-teal-500 to-teal-600", image: "/staff/charlo-casidsid.jpg" },
-      { name: "RAV S. CAPISTRANO", position: "Account Officer", icon: Briefcase, color: "from-orange-500 to-orange-600", image: "/staff/rav-capistrano.jpg" },
-      { name: "GERSHOM JAY P. ALENTAJAN", position: "Account Officer", icon: Briefcase, color: "from-orange-500 to-orange-600", image: "/staff/rav-capistrano.jpg" }
+      { name: "GLENN D. MISAJON", position: "Account Officer", icon: Briefcase, color: "from-indigo-500 to-indigo-600", image: "/Management%20and%20Staff/Main%20Office/Cash%20Section/glenn%20misajon.png" },
+      { name: "LAMBERT S. LABIAO", position: "Account Officer", icon: Briefcase, color: "from-red-500 to-red-600", image: "/Management%20and%20Staff/Main%20Office/Cash%20Section/lamber%20labiao.png" },
+      { name: "CHARLO C. CASIDSID", position: "Account Officer", icon: Briefcase, color: "from-teal-500 to-teal-600", image: "/Management%20and%20Staff/Main%20Office/Cash%20Section/charlo%20casidsid.png" },
+      { name: "RAV S. CAPISTRANO", position: "Account Officer", icon: Briefcase, color: "from-orange-500 to-orange-600", image: "" },
+      { name: "GERSHOM JAY P. ALENTAJAN", position: "Account Officer", icon: Briefcase, color: "from-orange-500 to-orange-600", image: "" }
     ],
     loanSection: [
-      { name: "JASMIN JOY N. MAGBANUA", position: "Clerk, Loan Processor", icon: FileText, color: "from-red-500 to-red-600", image: "/staff/jasmin-magbanua.jpg" },
-      { name: "CRISTY C. MAGBANUA", position: "Clerk, Loan Monitoring", icon: TrendingUp, color: "from-teal-500 to-teal-600", image: "/staff/cristy-magbanua.jpg" },
-      { name: "MA. RAFONCEL T. SANOY", position: "OIC Loan Officer", icon: Award, color: "from-orange-500 to-orange-600", image: "/staff/rafoncel-sanoy.jpg" },
-      { name: "WENDAI MAE ELIZALDE", position: "Clerk, JO", icon: UserCheck, color: "from-cyan-500 to-cyan-600", image: "/staff/wendai-elizalde.jpg" },
-      { name: "JULIUS E. CALANOG", position: "CIBI", icon: Shield, color: "from-lime-500 to-lime-600", image: "/staff/julius-calanog.jpg" }
+      { name: "JASMIN JOY N. MAGBANUA", position: "Clerk, Loan Processor", icon: FileText, color: "from-red-500 to-red-600", image: "/Management%20and%20Staff/Main%20Office/Loans/jasmine%20joy%20manbanua.png" },
+      { name: "CRISTY C. MAGBANUA", position: "Clerk, Loan Monitoring", icon: TrendingUp, color: "from-teal-500 to-teal-600", image: "/Management%20and%20Staff/Main%20Office/Loans/cristy%20magbanua.png" },
+      { name: "MA. RAFONCEL T. SANOY", position: "OIC Loan Officer", icon: Award, color: "from-orange-500 to-orange-600", image: "/Management%20and%20Staff/Main%20Office/Loans/rafoncel%20sanoy.png" },
+      { name: "WENDAI MAE ELIZALDE", position: "Clerk, JO", icon: UserCheck, color: "from-cyan-500 to-cyan-600", image: "/Management%20and%20Staff/Main%20Office/Loans/wendai%20mae%20elizalde.png" },
+      { name: "JULIUS E. CALANOG", position: "CIBI", icon: Shield, color: "from-lime-500 to-lime-600", image: "/Management%20and%20Staff/Main%20Office/Loans/julius%20calanog.png" }
     ],
     marketingAssistantSection: [
-      { name: "JOANN MAE T. LAZARO", position: "Marketing Assistant", icon: Megaphone, color: "from-teal-500 to-teal-600", image: "/staff/joann-lazaro.jpg" },
-      { name: "GLOREANNE P. MANA-AY", position: "Marketing Assistant", icon: Megaphone, color: "from-orange-500 to-orange-600", image: "/staff/gloreanne-mana-ay.jpg" },
-      { name: "NOEMI M. SITCHON", position: "Marketing Assistant", icon: Megaphone, color: "from-cyan-500 to-cyan-600", image: "/staff/noemi-sitchon.jpg" }
+      { name: "JOANN MAE T. LAZARO", position: "Marketing Assistant", icon: Megaphone, color: "from-teal-500 to-teal-600", image: "" },
+      { name: "GLOREANNE P. MANA-AY", position: "Marketing Assistant", icon: Megaphone, color: "from-orange-500 to-orange-600", image: "/Management%20and%20Staff/Main%20Office/Admin/GLOREANNE%20P.%20MANA-AY.jpg" },
+      { name: "NOEMI M. SITCHON", position: "Marketing Assistant", icon: Megaphone, color: "from-cyan-500 to-cyan-600", image: "" }
     ]
   };
 
