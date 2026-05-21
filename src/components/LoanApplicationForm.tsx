50707ffced853871dded775f5ea9e638c3322daa
Arjay18 <arjaydomingo008@gmail.com>
2026-05-21 13:30:55 +0800

    `Update dynamic import of LoanApplication component`

--- src/components/LoanApplicationForm.tsx
+++ src/components/LoanApplicationForm.tsx
@@ -4,9 +4,12 @@ import dynamic from 'next/dynamic';
 
 // Next.js app routes can't be imported as modules reliably.
 // Dynamically load the /online-application page as a component.
-const LoanApplication = dynamic(() => import("../app/online-application/page").then((m) => m.default), {
+const LoanApplication = dynamic(async () => {
+  // Load the client component without creating a static TS module import.
+  const mod: any = await import("../app/online-application/page");
+  return mod?.default;
+}, {
   ssr: false,
-  // Make sure this renders only on the client.
 });
 
 export default LoanApplication;
