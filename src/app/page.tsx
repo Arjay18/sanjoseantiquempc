13d72e088a9fb612ffdf77ad650afee26fd91ae3
Arjay18 <arjaydomingo008@gmail.com>
2026-05-26 11:07:18 +0800

    `Removed AboutUs component and NewsSlider, updated import and layout`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomeSlider from "@/components/HomeSlider";
 import NewsSlider from "@/components/NewsSlider";
 
 import ContactForm from "@/components/ContactForm";
-import AboutUs from "@/components/home/AboutUs";
+
 import LatestProjects from "@/components/home/LatestProjects";
 import Testimonials from "@/components/home/Testimonials";
 import PMESSchedule from "@/components/home/PMESSchedule";
@@ -20,8 +20,8 @@ export default function Home() {
         </FadeIn>
 
         <FadeIn direction="up" delay={0.1}>
-
           <div className="py-16 bg-gradient-to-br from-gray-50 via-white to-gray-50">
+
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <FadeIn direction="up" delay={0.1}>
                 <div className="text-center mb-12">
@@ -45,9 +45,8 @@ export default function Home() {
           </div>
         </FadeIn>
 
-        <FadeIn direction="up" delay={0.2}>
-          {/* News moved below ServicesShowcase */}
-        </FadeIn>
+        {/* spacer removed */}
+
 
         <FadeIn direction="up" delay={0.8}>
           <ServicesShowcase />
