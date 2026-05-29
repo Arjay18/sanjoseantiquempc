835561f232e0c2698d0dcc2368568249b7e92e91
Arjay18 <arjaydomingo008@gmail.com>
2026-05-29 08:40:54 +0800

    kjhdkfjshd

---
+++ src/app/evaluate/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import EvaluationForm from "@/components/EvaluationForm";
+
+export default function EvaluatePage() {
+  return (
+    <div className="w-full">
+      <EvaluationForm />
+    </div>
+  );
+}
+
