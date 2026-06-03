ae9aeeedd2b0e21213815bd647c1ace36a201990
Arjay18 <arjaydomingo008@gmail.com>
2026-06-03 13:28:10 +0800

    fix

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -102,8 +102,7 @@ export default function Navigation() {
     { name: 'Contact', href: '/contact' },
     {
       name: 'Coop Login',
-
-      href: '/login',
+      href: pathname === '/not-found' ? '/not-found' : '/login',
     },
   ];
 
